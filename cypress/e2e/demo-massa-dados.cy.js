f6b36132feb8a7a7ef6436a0ba3d38fe34d07d5c
Ana <aaraujo.morais@gmail.com>
2023-07-28 14:04:14 -0300

    Atualização com mais exemplos

--- cypress/e2e/demo-massa-dados.cy.js
+++ cypress/e2e/demo-massa-dados.cy.js
@@ -1,10 +1,11 @@
-describe('Demonstração de criação de arquivo com dados aleatórios e seu consumo', () => {
+describe('Demonstração de criação de arquivo com dados fictícios e seu consumo', () => {
   before(() => {
-    cy.gerarMassaAutomatica();
+    cy.gerarVarios();
+    cy.gerarApenasUm();
     //cy.visit('/');
   });
-  it.skip('Exemplo de como consumir dados do arquivo', () => {
-    var usuarios = require('../fixtures/nome-do-arquivo.json');
+  it.skip('Exemplo de como consumir dados de um arquivo com vários objetos', () => {
+    var usuarios = require('../fixtures/Dados2.json');
     usuarios.forEach(usuario => {
       cy.get('input[name="nome"]').type(`${usuario.nome}`);
       cy.get('input[name="cpf"]').type(`${usuario.cpf}`);
@@ -17,4 +18,17 @@ describe('Demonstração de criação de arquivo com dados aleatórios e seu con
       cy.url().should('eq', '/success');
     });
   });
-})
+
+  it.skip('Exemplo de como consumir dados de um arquivo com apenas um objeto', () => {
+    var usuario = require('../fixtures/Dados1.json');
+    cy.get('input[name="nome"]').type(`${usuario.nome}`);
+    cy.get('input[name="cpf"]').type(`${usuario.cpf}`);
+    cy.get('input[placeholder="dd/mm/yyyy"]').type(`${usuario.data_nasc}`);
+    cy.selecionaUfIndicado(`${usuario.uf}`);
+    cy.get('input[name="cidade"]').type(`${usuario.cidade}`);
+    cy.get('input[name="telefone"]').type(`${usuario.celular}`);
+    cy.get('input[name="email"]').type(`${usuario.email}`);
+    cy.get('button').contains('Enviar').click();
+    cy.url().should('eq', '/success');
+  });
+});
--- cypress/support/gerarDados.js
+++ cypress/support/gerarDados.js
@@ -3,11 +3,11 @@ import faker from 'faker-br';
 import { gerarDataNascimento, gerarDataNascISO } from './utils';
 
 //Criação de comando customizado
-Cypress.Commands.add('gerarMassaAutomatica', () => {
-    cy.writeFile('cypress/fixtures/nome-do-arquivo.json',
+Cypress.Commands.add('gerarVarios', () => {
+    cy.writeFile('cypress/fixtures/Dados2.json',
         Cypress._.times(5, () => {
             let dataNascimento = gerarDataNascimento();
-            let dataNascimento2 = gerarDataNascISO(gerarDataNascimento());          
+            let dataNascimento2 = gerarDataNascISO(gerarDataNascimento());
 
             const nome = faker.name.firstName() + ' ' + faker.name.lastName();
             const cpf = faker.br.cpf();
@@ -20,12 +20,37 @@ Cypress.Commands.add('gerarMassaAutomatica', () => {
                 'nome': `${nome}`,
                 'cpf': `${cpf}`,
                 'data_nasc': `${dataNascimento}`,
-                'data_nascISO':`${dataNascimento2}`,
+                'data_nascISO': `${dataNascimento2}`,
                 'uf': `${uf}`,
                 'cidade': `${cidade}`,
                 'celular': `${celular}`,
                 'email': `${email}`
             }
         })
+    )
+})
+
+Cypress.Commands.add('gerarApenasUm', () => {
+    let dataNascimento = gerarDataNascimento();
+    let dataNascimento2 = gerarDataNascISO(gerarDataNascimento());
+
+    const nome = faker.name.firstName() + ' ' + faker.name.lastName();
+    const cpf = faker.br.cpf();
+    const uf = faker.address.state();
+    const cidade = faker.address.city();
+    const celular = faker.phone.phoneNumber();
+    const email = faker.internet.email();
+
+    cy.writeFile('cypress/fixtures/Dados1.json',
+        {
+            'nome': `${nome}`,
+            'cpf': `${cpf}`,
+            'data_nasc': `${dataNascimento}`,
+            'data_nascISO': `${dataNascimento2}`,
+            'uf': `${uf}`,
+            'cidade': `${cidade}`,
+            'celular': `${celular}`,
+            'email': `${email}`
+        }
     )
 })
